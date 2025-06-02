c59816a8ed76d4bd7913fa59fef5772fcb4705b0
EDivkovic <ed.divkovic@student.avans.nl>
2025-06-02 19:34:06 +0200

    Setup .env

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("It works!");
 });
 
+console.log("My port: ", process.env.PORT);
 
-// const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port:", PORT);
 });
