dba1ab8e48b6f32bc3f4266378853ca23fe42894
EDivkovic <ed.divkovic@student.avans.nl>
2025-06-06 00:34:45 +0200

    Created post route for transactions

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 async function initDB() {
@@ -26,9 +28,25 @@ async function initDB() {
     }
 };
 
-app.get("/", (req, res) => {
-    res.send("It works!");
-});
+app.post("/api/transactions", async (req, res) => {
+    const { userId, title, amount, category } = req.body;
+
+    if (!userId || !title || amount == undefined || !category) {
+        return res.status(400).json({ error: "All fields are required"});
+    }
+
+    try {
+        const result = await sql`
+        INSERT INTO transactions (userId, title, amount, category) 
+        VALUES (${userId}, ${title}, ${amount}, ${category}) 
+        RETURNING *`
+
+        res.status(201).json(result[0]);
+    } catch (error) {
+        console.error("Error creating transaction:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+})
 
 console.log("My port: ", process.env.PORT);
 
